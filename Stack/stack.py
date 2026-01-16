2b179f5ee0418c94b97b3764077cb37c1569ddcc
Aryan <aryankawale163@gmail.com>
2026-01-16 21:26:27 +0530

    feat: Implement a stack data structure in Python within `stack.py`.

--- Stack/stack.py
+++ Stack/stack.py
@@ -1,29 +1,33 @@
 class stack:
     def __init__(self):
         self.s = []
-    
+
     def length(self):
         return len(self.s)
 
     def push(self,value):
         self.s.insert(0,value)
+        # self.s.append(value)
 
     def  peek(self):
         if len(self.s) == 0:
             raise Exception("Stack is empty")
         else:
             return self.s[0]
-        
+            # return self.s[-1]
+
     def pop(self):
         if len(self.s) == 0:
             raise Exception("Stack is empty")
         else:
             return self.s.pop(0)
+            # return self.s.pop()
 
 stk = stack()
 stk.push(10)
 stk.push(20)
 stk.push(30)
-print(stk.pop())
-print(stk.pop())
-print(stk.pop())
+print(stk.peek())
+# print(stk.pop())
+# print(stk.pop())
+# print(stk.pop())
