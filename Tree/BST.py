54f901fb580886298c29dcef25f1a4b187a381d0
Aryan <aryankawale163@gmail.com>
2026-02-14 21:20:59 +0530

    feat: Implement a Binary Search Tree (BST) with insert, search, and in-order traversal functionalities.

--- Tree/BST.py
+++ Tree/BST.py
@@ -5,30 +5,30 @@ class Node:
         self.data = value
     
 def insert(root,value):
-    if(root == None):
+    if root == None:
         return Node(value)
-    if(root.data == value):
+    if root.data == value:
         return root
-    if(root.data > value):
+    if root.data > value:
         root.left = insert(root.left,value)
     else:
         root.right = insert(root.right,value)
     return root
 
 def search(root,value):
-    if(root == None):
+    if root == None:
         print("Element not found", end="\n")
         return
-    if(root.data == value):
+    if root.data == value:
         print("Element found", end="\n")
         return
-    if(root.data > value):
+    if root.data > value:
         search(root.left,value)
     else:
         search(root.right,value)
 
 def inOrder(root):
-    if(root != None):
+    if root != None:
         inOrder(root.left)
         print(root.data, end=" ")
         inOrder(root.right)
