5181f7208c8bea2fb71822b8dc1cf0bff4fdf100
Aryan <aryankawale163@gmail.com>
2026-04-09 22:18:08 +0530

    feat: add recursive implementations for factorial and Fibonacci sequences

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -1,6 +1,12 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * factorial(n-1)
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     return n * factorial(n-1)
 
-print(factorial(4))
+# print(factorial(4))
+
+num = 5
+fact=1
+for i in range(1,num+1):
+    fact = fact*i
+print(f"Factorial of {num} is {fact}")  
