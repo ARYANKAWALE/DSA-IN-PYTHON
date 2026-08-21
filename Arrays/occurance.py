a99fb4597d90d6792be52dfc16e134c32007cff5
Aryan <aryankawale163@gmail.com>
2026-08-21 23:05:32 +0530

    feat: add Python script to count element occurrences in an array

---
+++ Arrays/occurance.py
@@ -0,0 +1,43 @@
+input = [2,5,3,2,6,7,2,6,4,6,2]
+count = 0
+target = 6
+
+for i in input:
+    if i == target:
+        count+=1
+print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# target = 2
+# left = 0
+# occurance = []
+# right = len(input) - 1
+# while left < right:
+    
