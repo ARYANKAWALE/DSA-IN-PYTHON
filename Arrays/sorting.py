5eac6ed7657dc233c0acd010c9469c8c65a86c35
Aryan <aryankawale163@gmail.com>
2026-09-02 23:27:02 +0530

    feat: add array utility scripts for moving zeros to the end and solving two-sum via sorting

---
+++ Arrays/sorting.py
@@ -0,0 +1,18 @@
+def twoSum(arr,target):
+    arr.sort()
+    left = 0
+    right = len(arr) - 1
+    while(left < right):
+        current_sum = arr[left] + arr[right]
+        if (current_sum == target):
+            return [left,right]
+        elif (current_sum < target):
+            left += 1
+        elif (current_sum > target):
+            right -= 1
+    return "Empty error"
+arr = [5,8,7,2]
+target = 12
+print(arr , "unsorted")
+print(arr, "sorted")
+print(twoSum(arr,target))
